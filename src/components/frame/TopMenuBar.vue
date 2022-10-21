258191579e8fb2493af8babe9859e8ccd05143ed
dokyung oh <dko0025@naver.com>
2022-10-21 14:21:30 +0900

    sdfdsf

---
+++ src/components/frame/TopMenuBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div  class="topmenubar_title float-right mx-3 my-1">this is top menu bar (로그인 / 회원가입)</div>
+</template>
+
+<script>
+export default {
+    name: 'TopMenuBar',
+}
+</script>
+
+<style scoped="scss">
+.topmenubar_title {
+    text-align: right;
+}
+</style>
